f409e7dbfe38e9ed2c7a4bc543a04a3571786027
Rachel Mandelbaum <rlmandelbaum@gmail.com>
2017-08-27 22:19:46 +0000

    hard coded scraper class for list of location names

--- lib/Smorgasburg/cli.rb
+++ lib/Smorgasburg/cli.rb
@@ -9,37 +9,40 @@ class Smorgasburg::CLI
 def list_locations
   #first hard code to make sure it works, then use scrape to create objects
   puts "Please enter the number to see more info of the location."
-  puts "1. Smorgasburg Prospect Park"
-  puts "2. Smorgasburg Williamsburg"
-  puts "3. Smorgasburg Square"
 
-  input = gets.strip
-  index = input.to_i - 1
-
-  if !index.between?(0,2)
-    puts "Invalid input"
-    list_locations
-
-  else
-    puts "#{smorg.name} - #{smorg.day} - #{smorg.time} - #{smorg.address}"
-
+  @locations = Smorgasburg::Scraper.details
+  @locations.each.with_index(1) do |local, i|
+    puts " #{i}. #{local.name}"
   end
-
-def more_options
-  puts "To see more info of another location enter 'see more'. Otherwise enter 'exit.'"
-  input = gets.strip
-
-  if input != "exit"
-    list_locations
-  else
-    "Goodbye!"
-  end
-
 end
+#
+#   input = gets.strip
+#   index = input.to_i - 1
+#
+#   if !index.between?(0,2)
+#     puts "Invalid input"
+#     list_locations
+#
+#   else
+#     puts "#{smorg.name} - #{smorg.day} - #{smorg.time} - #{smorg.address}"
+# end
+#   end
+#
+# def more_options
+#   puts "To see more info of another location enter 'see more'. Otherwise enter 'exit.'"
+#   input = gets.strip
+#
+#   if input != "exit"
+#     list_locations
+#   else
+#     "Goodbye!"
+#   end
+#
+# end
 
 
 
-end
+
 
 def more_options
 end
--- lib/Smorgasburg/scraper.rb
+++ lib/Smorgasburg/scraper.rb
@@ -1,5 +1,29 @@
 class Smorgasburg::Scraper
 
+attr_accessor :name, :day , :time , :address
+
+def self.details
+  location_1 = self.new
+  location_1.name = "Smorgasburg Prospect Park"
+  location_1.day = "Sunday"
+  location_1.time = "11am -6pm"
+  location_1.address = "Prospect Park-Well House"
+
+  location_2 = self.new
+  location_2.name = "Smorgasburg Williamsburg"
+  location_2.day = "Saturday"
+  location_2.time = "11am -6pm"
+  location_2.address = "Williamsburg N7th Street"
+
+  location_3 = self.new
+  location_3.name = "Smorgasburg Square"
+  location_3.day = "Saturday and Sunday"
+  location_3.time = "11am -6pm"
+  location_3.address = "IDK"
+
+  [location_1,location_2,location_3]
 
 
-end 
+
+end
+end
