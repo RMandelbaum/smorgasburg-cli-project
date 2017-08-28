f0e1312a94f39cc8051561501629c613a2076f60
Rachel Mandelbaum <rlmandelbaum@gmail.com>
2017-08-28 00:11:17 +0000

    scraped address

--- lib/Smorgasburg/scraper.rb
+++ lib/Smorgasburg/scraper.rb
@@ -22,11 +22,11 @@ def self.scrape_williamsburg
   html = open("https://www.smorgasburg.com/saturday-williamsburg/")
   doc = Nokogiri::HTML(html)
    location = self.new
-#
+
    location.name = doc.css("div .project-slide-description-text p").first.text
    location.time = doc.css("div .project-slide-description-text p + p").first.text
-#   location.address = doc.css("div .project-slide-description-text p p p").text
-  location
+   location.address = doc.css("div .project-slide-description-text p + p + p").first.text
+   location
   end
 #
  def self.scrape_prospect_park
@@ -36,19 +36,19 @@ def self.scrape_williamsburg
 #
    location.name = doc.css("div .project-slide-description-text p").first.text
    location.time = doc.css("div .project-slide-description-text p + p").first.text
-#   location.address = doc.css("")
-  location
+   location.address = doc.css("div .project-slide-description-text p + p + p").first.text
+   location
  end
-# #
+
   def self.scrape_square
     html = open("https://www.smorgasburg.com/smorgasburg-varick/")
     doc = Nokogiri::HTML(html)
     location = self.new
 
     location.name = doc.css("div .project-slide-description-text p").first.text
-   location.time = doc.css("div .project-slide-description-text p + p").first.text
-# #   location.address = doc.css("")
-   location
+    location.time = doc.css("div .project-slide-description-text p + p").first.text
+    location.address = doc.css("div .project-slide-description-text p + p + p").first.text
+    location
   end
 
 end
